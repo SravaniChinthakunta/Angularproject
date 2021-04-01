3aa41fc387824a3b2d7c7e0a93d54691f7315807
SravaniChinthakunta <sravanichinthakunta97@gmail.com>
2021-04-01 22:13:57 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BreakfastComponent } from './breakfast/breakfast.component';
+import { FastfoodComponent } from './fastfood/fastfood.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+import { SaladsComponent } from './salads/salads.component';
+import { SnacksComponent } from './snacks/snacks.component';
+import { SoftdrinksComponent } from './softdrinks/softdrinks.component';
+import { StartersComponent } from './starters/starters.component';
+
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"breakfast",component:BreakfastComponent},
+  {path:"starters",component:StartersComponent},
+
+  {path:"salads",component:SaladsComponent},
+  {path:"snacks",component:SnacksComponent},
+  {path:"softdrinks",component:SoftdrinksComponent},
+  {path:"fastfood",component:FastfoodComponent},];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BreakfastComponent } from './breakfast/breakfast.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { FastfoodComponent } from './fastfood/fastfood.component';
+import { FooterComponent } from './footer/footer.component';
+import { SaladsComponent } from './salads/salads.component';
+import { SnacksComponent } from './snacks/snacks.component';
+import { SoftdrinksComponent } from './softdrinks/softdrinks.component';
+import { StartersComponent } from './starters/starters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BreakfastComponent,
+    CarouselComponent,
+    FastfoodComponent,
+    FooterComponent,
+    SaladsComponent,
+    SnacksComponent,
+    SoftdrinksComponent,
+    StartersComponent
   ],
   imports: [
     BrowserModule,
